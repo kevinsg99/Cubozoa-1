e8bf8a988ee156670a50a0cccf3458af3b761cf5
Gary Isufi <isufi.2@buckeyemail.osu.edu>
2019-11-02 12:55:48 -0400

    first commit

--- project.py
+++ project.py
@@ -1,10 +1,11 @@
 import googlemaps
+import geocoder
 from datetime import datetime
 from flask import Flask
 
 app = Flask(__name__)
 
-gmaps = googlemaps.Client(key=AIzaSyAsLI9pzus4z91Pyq1_aANnpOa8YKzE2t8)
+gmaps = googlemaps.Client(key="AIzaSyAsLI9pzus4z91Pyq1_aANnpOa8YKzE2t8")
 
 # Geocoding an address
 geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
@@ -18,3 +19,6 @@ directions_result = gmaps.directions("Sydney Town Hall",
                                      "Parramatta, NSW",
                                      mode="transit",
                                      departure_time=now)
+
+g = geocoder.ip('me')
+print(g.latlng)
