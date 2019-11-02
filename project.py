f33f065952f60ff5a62c3fcf042d05a98e6885cf
kevinsg99 <kevinsg99@gmail.com>
2019-11-02 13:53:26 -0400

    Update project.py

--- project.py
+++ project.py
@@ -8,17 +8,17 @@ app = Flask(__name__)
 gmaps = googlemaps.Client(key='AIzaSyAsLI9pzus4z91Pyq1_aANnpOa8YKzE2t8')
 
 # Geocoding an address
-geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+#geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
 
 # Look up an address with reverse geocoding
 reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
 
 # Request directions via public transit
-now = datetime.now()
-directions_result = gmaps.directions("Sydney Town Hall",
-                                     "Parramatta, NSW",
-                                     mode="transit",
-                                     departure_time=now)
+#now = datetime.now()
+#directions_result = gmaps.directions("Sydney Town Hall",
+#                                     "Parramatta, NSW",
+#                                     mode="transit",
+#                                     departure_time=now)
 
 #gets current location, returning array of coordinates
 def getCurrentLocation():
