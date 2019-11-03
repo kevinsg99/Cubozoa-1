adb9d0d40d5d9054704a688aa6cff1eb54909f47
noahl1 <you@example.com>
2019-11-03 05:08:46 -0500

    weqfa

--- project.py
+++ project.py
@@ -5,16 +5,17 @@ from flask import Flask, render_template
 from queue import LifoQueue
 from googlemaps import convert
 from googlemaps import directions
+import socket
 
 app = Flask(__name__)
-
 gmaps = googlemaps.Client(key="AIzaSyAsLI9pzus4z91Pyq1_aANnpOa8YKzE2t8")
-mq_KEY = "iDccP1485vRS7TK0BzhNqXc6fro7ckuD"
-
+mq_KEY = 'iDccP1485vRS7TK0BzhNqXc6fro7ckuD'
+socket.setdefaulttimeout(9)
 @app.route("/")
 def home():
-    data = open('index.html').read()    
-    return data
+    #data = open('templates/index.html').read()    
+    #return data
+    return render_template('index.html')
 
 home = [0.00, 0.00]
 coordinates = [0.00,0.00]
@@ -24,43 +25,61 @@ coordinates = [0.00,0.00]
 def sweep(home, spacing):
     grid = [home]
     row=1
+    #1 quadrant
     while 30*row < 5280*runDistance/2.0:
         column=1
         while 30*column < 5280*runDistance/2.0:
             grid.append([home[0]+spacing*column,home[1]])
             row+=1
+            column+=1
         grid.append([home[0],home[1]+column*spacing])
-        column+=1
+        
+    row=1
+    print('1Q'+str(grid))
+    #4th quadrant
+    while 30*row < 5280*runDistance/2.0:
+        column=1
+        while 30*column < 5280*runDistance/2.0:
+            grid.append([home[0]+spacing*column,home[1]])
+            row+=1
+            column+=1
+        grid.append([home[0],home[1]-column*spacing])
+        
+    row=1
+    print('4Q'+str(grid))
+    #3rd quadrant
     while 30*row < 5280*runDistance/2.0:
         column=1
         while 30*column < 5280*runDistance/2.0:
             grid.append([home[0]-spacing*column,home[1]])
             row+=1
-        grid.append([home[0],home[1]+column*spacing])
-        column+=1
+            column+=1
+        grid.append([home[0],home[1]-column*spacing])      
+    row=1
+    print('3Q'+str(grid))
+    #2nd quadrant
     while 30*row < 5280*runDistance/2.0:
         column=1
-        while 30*column > -5280*runDistance/2.0:
-            grid.append([home[0]+spacing*column,home[1]])
-            row+=1
-        grid.append([home[0],home[1]-column*spacing])
-        column+=1
-    while 30*row > -5280*runDistance/2.0:
-        column=1
-        while 30*column > 5280*runDistance/2.0:
+        while 30*column < 5280*runDistance/2.0:
             grid.append([home[0]-spacing*column,home[1]])
             row+=1
-        grid.append([home[0],home[1]-column*spacing])
-        column+=1
+            column+=1
+        grid.append([home[0],home[1]+column*spacing])
+    print('2Q'+str(grid))
+
     intersections = []
+
     for i in range(len(grid)):
+        '''
         requested_info = requests.get("http://www.mapquestapi.com/geocoding/v1/reverse?key="
         + mq_KEY + "&location=" 
         + str(grid[i][0])
         + ","
         + str(grid[i][1])
         + "&includeRoadMetadata=true&includeNearestIntersection=true")
-        intersection_coordinates = [requested_info.json()['results'][0]['providedLocation']['latLng']['lat'],requested_info.json()['results'][0]['providedLocation']['latLng']['lng']]
+        '''
+        requested_info = geocoder.mapquest([grid[i][0], grid[i][1]],method='reverse',key=mq_KEY)
+        intersection_coordinates = [requested_info.json()['results'][0]['locations'][0]['nearestIntersection']['latLng']['latitude'],requested_info.json()['results'][0]['locations'][0]['nearestIntersection']['latLng']['longitude']]
         intersections.append(intersection_coordinates)
     print(intersections)
     return intersections
@@ -79,21 +98,11 @@ def getCurrentLocation():
 #print(coordinates)
 #home = [{"lat" : coordinates[0], "lng" : coordinates[1]}, "Columbus"]
 #home = (coordinates[0], coordinates[1])
-home = (40.006066, -83.009263)
-neighbor1 = (40.026252, -83.027001)
-neighbor2 = (40.025054, -83.041613)
-neighbor3 = (40.052468, -83.041610)
-route = LifoQueue()
-allRoutes = LifoQueue()
-totalDist = 0
-streetDist = 1
-runDistance = 4
-#adjStreet = gmaps.nearest_roads(home)[0]
-adjStreet = []
-adjStreet.append(gmaps.nearest_roads(home)[0])
-adjStreet.append(gmaps.nearest_roads(neighbor1)[0])
-adjStreet.append(gmaps.nearest_roads(neighbor2)[0])
-adjStreet.append(gmaps.nearest_roads(neighbor3)[0])
+
+#adjStreet.append(gmaps.nearest_roads(home)[0])
+#adjStreet.append(gmaps.nearest_roads(neighbor1)[0])
+#adjStreet.append(gmaps.nearest_roads(neighbor2)[0])
+#adjStreet.append(gmaps.nearest_roads(neighbor3)[0])
 #print(adjStreet)
 '''
 addressInfo = gmaps.reverse_geocode(home)
@@ -102,7 +111,7 @@ streetAddress = gmaps.reverse_geocode((streetCoords['latitude'], streetCoords['l
 street = streetAddress[1]['address_components'][1]['long_name']
 '''
 #print(street)
-direct = gmaps.directions(home, neighbor1)
+#direct = gmaps.directions(home, neighbor1)
 
 def getMiles(direct):
     directLegs = direct[0]['legs']
@@ -119,7 +128,6 @@ def getMiles(direct):
     return miles
 
 def router(adjacent):
-    print("hi")
     totalDist = 0
     while len(adjacent) > 0:
         n = adjacent[0]
@@ -143,5 +151,11 @@ def router(adjacent):
 
 if __name__ == "__main__":
     app.run(debug=False)
+    home = (40.006066, -83.009263)
+    route = LifoQueue()
+    allRoutes = LifoQueue()
+    totalDist = 0
+    streetDist = 1
+    runDistance = 4
+    adjStreet = sweep(home,0.00005)
     router(adjStreet)
-    sweep(home, 0.005)
